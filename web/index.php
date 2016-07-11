665fc27131de7e4ffc607e4819f9617dd5c1ab89
Olivier Laviale <olivier.laviale@gmail.com>
2016-07-11 21:29:13 +0200

    Added Disqus comments

--- web/index.php
+++ web/index.php
@@ -9,5 +9,8 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . $_SERVER["REQUEST_URI"
 	return false;
 }
 
+//$log = json_encode([ $_SERVER['REQUEST_TIME_FLOAT'], $_SERVER['REMOTE_ADDR'], $_SERVER['REQUEST_URI']]);
+//file_put_contents(__DIR__ . '/../access.log', $log . PHP_EOL, FILE_APPEND);
+
 $app = require __DIR__ . '/../bootstrap.php';
 $app();
