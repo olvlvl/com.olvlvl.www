6563e56771554efad55be741accf66ea1a37f5c9
Olivier Laviale <olivier.laviale@gmail.com>
2017-02-05 22:57:13 +0100

    Add Expire for pages

--- lib/Presentation/Controller/PageController.php
+++ lib/Presentation/Controller/PageController.php
@@ -10,6 +10,8 @@ class PageController extends ControllerAbstract
 
 	protected function action_about()
 	{
+		$this->response->cache_control = 'public';
+		$this->response->expires = '+3 hour';
 		$this->view->template = 'page/show';
 		$this->view->content = $this->app->template_engines->render('_pages/about.md', null, []);
 		$this->view['page_title'] = "About Olivier Laviale";
