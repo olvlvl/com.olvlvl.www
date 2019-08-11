80319612ccd38587fb8773a43f64ff529b852805
Olivier Laviale <olivier.laviale@gmail.com>
2019-08-11 15:14:41 +0200

    Improve rendering performance

--- web/assets/page.js
+++ web/assets/page.js
@@ -1,16 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+!function() {
 
 	var links = document.body.querySelectorAll('[href^="http"]')
 
-	for (var i = 0, j = links.length ; i < j ; i++)
-	{
+	for (var i = 0, j = links.length ; i < j ; i++) {
 		links[i].target = '_blank'
 	}
 
-	balanceText(document.body.querySelector('h1'), { watch: true })
-
-	document.body.querySelectorAll('h2').forEach(function (el) {
-		balanceText(el, { watch: true })
+	window.addEventListener('load', function () {
+		balanceText('h1, h2', { watch: true })
 	})
 
-})
+} ()
