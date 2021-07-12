efe046e32c059641cc9570afb7083ff9b9606132
Olivier Laviale <olivier.laviale@gmail.com>
2021-07-12 09:27:56 +0200

    Optimize JS links

--- web/assets/ga.js
+++
@@ -1,4 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-8673332-6');
--- web/assets/page.js
+++ web/assets/page.js
@@ -1,13 +1,11 @@
 !function() {
+	const links = document.body.querySelectorAll('[href^="http"]')
 
-	var links = document.body.querySelectorAll('[href^="http"]')
-
-	for (var i = 0, j = links.length ; i < j ; i++) {
+	for (let i = 0, j = links.length ; i < j ; i++) {
 		links[i].target = '_blank'
 	}
 
 	window.addEventListener('load', function () {
 		balanceText('h1, h2', { watch: true })
 	})
-
 } ()
