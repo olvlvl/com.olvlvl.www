ad98e686412a552c79deec6c10434b8a1a6eb510
Olivier Laviale <olivier.laviale@gmail.com>
2023-02-26 17:27:31 +0100

    Differ GTM

--- assets/page.js
+++ assets/page.js
@@ -1,13 +1,33 @@
 (function() {
-	window.addEventListener('DOMContentLoaded', ev => {
+	function updateLinkTargets() {
 		const links = document.body.querySelectorAll('[href^="http"]')
 
 		for (let i = 0, j = links.length ; i < j ; i++) {
 			links[i].target = '_blank'
 		}
+	}
+
+	function ga () {
+		const el = document.createElement('script')
+
+		el.src = "https://www.googletagmanager.com/gtag/js?id=G-1HJ8TFRQ47"
+		el.async = true
+
+		document.head.appendChild(el)
+
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-1HJ8TFRQ47');
+	}
+
+	ga()
+
+	window.addEventListener('DOMContentLoaded', ev => {
+		updateLinkTargets()
 	})
 
 	window.addEventListener('load', ev => {
-		textBalancer.initialize('h1, h2');
+		textBalancer.initialize('h1, h2')
 	})
 }) ();
